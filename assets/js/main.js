d59a6b07a05dca4c3ec6d8321f155a1ee7d2e5ca
Lamia Rizk <lamiarizk@gmx.de>
2024-09-20 14:53:10 +0200

    bearbeitet

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// leider keine Zeit mehr, hier Überlegungen
+
+// values festlegen
+let body-size = parseInt()
+
+let age
+
+let weight
+
+let activity
+
+const gender
+
+// dann Grundbedarf errechnen, den *value activity (PAL-Faktor)
+// function math()
